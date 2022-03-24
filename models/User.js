453d0e580492b35800d8fd43b5cf65e6ad5df1b1
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-25 07:16:02 +0900

    Add User model

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { first: { type: String }, last: { type: String } },
+  username: { type: String, minlength: 5, required: true },
+  membershipStatus: { type: String, default: 'regular' },
+});
+
+module.exports = mongoose.model('User', UserSchema);
