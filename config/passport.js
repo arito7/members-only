6fef317fe9990140b98b471fd2d5221bfb6bd470
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-25 07:17:22 +0900

    Setup passport

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+const localStrategy = new LocalStrategy((username, password, done) => {
+  User.findOne({ username: username }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect Username' });
+    }
+    bcrypt.compare(pass, user.password, (err, res) => {
+      if (res) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect Password' });
+      }
+    });
+    return done(null, user);
+  });
+});
+
+passport.use(localStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
