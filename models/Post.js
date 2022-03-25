b6e6a2a873ed850e9748189967eaeff69adf4f36
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-26 05:07:23 +0900

    Add length validation on title and body

--- models/Post.js
+++ models/Post.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, length: { min: 3 } },
   timestamp: { type: Date, default: Date.now() },
-  body: { type: String, required: true },
+  body: { type: String, required: true, length: { min: 3, max: 200 } },
   creatorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
