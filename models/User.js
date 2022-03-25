4b0be7582787b9574a1c14a0f73c689205f857f0
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-25 09:13:41 +0900

    Update User with hash and salt fields

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { first: { type: String }, last: { type: String } },
   username: { type: String, minlength: 5, required: true },
+  hash: { type: String, required: true },
+  salt: { type: String, required: true },
   membershipStatus: { type: String, default: 'regular' },
 });
 
