4f10e77adfd532cf397df42b2dfbdf782d9e497c
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-26 07:16:57 +0900

    Make username lowercase

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: { first: { type: String }, last: { type: String } },
-  username: { type: String, minlength: 5, required: true },
+  username: { type: String, minlength: 5, required: true, lowercase: true },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
   membershipStatus: { type: String, default: 'regular' },
