b43d34c7aaa85f10543fea20959a21c072ddd31e
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-26 07:18:23 +0900

    Rename creatorId property to creator

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const PostSchema = new Schema({
   title: { type: String, required: true, length: { min: 3 } },
   timestamp: { type: Date, default: Date.now() },
   body: { type: String, required: true, length: { min: 3, max: 200 } },
-  creatorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
