7e9dfac040cb8fb33f680cb95321595a1b26fd07
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-26 09:27:35 +0900

    Refactoring

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const MongoStore = require('connect-mongo');
 // DB
 require('./config/mongodb');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,9 +41,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/', authRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
