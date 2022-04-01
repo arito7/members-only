f333810fc8047965642bcac0a0538d00b739addb
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-01 22:27:50 +0900

    Implement posts route

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ require('./config/mongodb');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
@@ -43,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', authRouter);
 app.use('/', indexRouter);
+app.use('/posts', postsRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
