6e94bc8f437559bfb9c52c8ddc7780d1139488ec
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-01 22:53:43 +0900

    Refactor out upgrade route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var users = express.Router();
+require('dotenv').config();
+const express = require('express');
+const users = express.Router();
+const { passcodeValidationSchema } = require('../config/validationSchemas');
+const User = require('../models/User');
 
 users.get('/', (req, res, next) => {
   res.render('template', { partial: 'user', data: {} });
@@ -7,6 +10,30 @@ users.get('/', (req, res, next) => {
 
 users.get('/admin', (req, res, next) => {
   res.render('template', { partial: 'admin', data: {} });
+});
+
+users.post('/upgrade', passcodeValidationSchema, (req, res, next) => {
+  if (process.env.PASSCODE === req.body.secret) {
+    User.findByIdAndUpdate(req.user.id, { membershipStatus: 'member' }).exec(
+      (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        res.locals.currentUser = user;
+        res.render('template', {
+          partial: 'user',
+          data: {
+            messages: [
+              {
+                type: 'success',
+                message: 'Congratulation You are now a member!',
+              },
+            ],
+          },
+        });
+      }
+    );
+  }
 });
 
 module.exports = users;
